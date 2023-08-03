7b790debe715382e39692c30150804de2557c9eb
j-doka <jdoka42@gmail.com>
2023-08-03 20:49:54 +0100

    change title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Deverb',
-  description: 'Prod By Deverb',
+  title: 'Fitters',
+  description: 'Find your next fit',
 }
 
 
